03c63d4cfa3bd91e0b4507fb86f9524da8f15c14
Maxime <maxime.caron@epitech.eu>
2024-06-20 12:46:31 +0200

    [+] Ajout d'un background personnalisé en fonction du type du pokémon.

--- Hop3Dex/app/helpers/pokemons_helper.rb
+++ Hop3Dex/app/helpers/pokemons_helper.rb
@@ -1,2 +1,26 @@
 module PokemonsHelper
+  def background_class_for_type(type)
+    case type.downcase
+    when 'feu'
+      'bg-red-500'
+    when 'eau'
+      'bg-blue-500'
+    when 'plante'
+      'bg-green-500'
+    when 'électrik'
+      'bg-yellow-500'
+    when 'psy'
+      'bg-purple-500'
+    when 'glace'
+      'bg-blue-300'
+    when 'dragon'
+      'bg-indigo-700'
+    when 'ténèbres'
+      'bg-gray-800'
+    when 'fée'
+      'bg-pink-400'
+    else
+      'bg-gray-200'
+    end
+  end
 end
--- Hop3Dex/config/tailwind.config.js
+++ Hop3Dex/config/tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'red-500': '#f56565',
+        'blue-500': '#4299e1',
+        'green-500': '#48bb78',
+        'yellow-500': '#ecc94b',
+        'purple-500': '#9f7aea',
+        'blue-300': '#63b3ed',
+        'indigo-700': '#4c51bf',
+        'gray-800': '#2d3748',
+        'pink-400': '#ed64a6',
+        'gray-200': '#e2e8f0',
+      },
     },
   },
   plugins: [
