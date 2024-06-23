7862970f2ddd4a6ad180c39939f105629b5b714b
Maxime <maxime.caron@epitech.eu>
2024-06-23 20:13:36 +0200

    [*] Modification du fichier build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Install gems without the --deployment option
-bundle install --path vendor/bundle
+# Exit immediately if a command exits with a non-zero status
+set -e
 
-# Continue with the default build process
+# Configure Bundler to use the vendor/bundle path
+bundle config set path 'vendor/bundle'
+
+# Install gems without the --deployment option
+bundle install
+
+# Precompile assets
 bundle exec rails assets:precompile
